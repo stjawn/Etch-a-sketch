9bc11ff6e60a2d90ab6cea08095d004406c26a93
stjawn <stjawn@protonmail.com>
2021-03-26 16:07:44 -0400

    Added buttons to shake and resize grid.

--- script.js
+++ script.js
@@ -1,45 +1,69 @@
- 
-
 const mainContainer = document.createElement("div");
 mainContainer.id = "main";
 document.body.appendChild(mainContainer);
 
-createGrid();
+const gridContainer = document.createElement("div");
+gridContainer.id = "grid";
+gridContainer.style.display = "grid";
+
+
+const sizeButton = document.createElement("button");
+sizeButton.id = "sizeButton";
+sizeButton.textContent = "Resize";
+sizeButton.addEventListener("click", () => {
+    changeSize();
+})
+mainContainer.appendChild(sizeButton);
+
+const shakeButton = document.createElement("button");
+shakeButton.id = "shakeButton";
+shakeButton.textContent = "Shake!"
+shakeButton.addEventListener("click", () => {
+    shake();
+})
+mainContainer.appendChild(shakeButton);
+
+
+createGrid(16);
 
 
 
 // Functions
 
-function createGrid() {
-    const gridContainer = document.createElement("div");
-    gridContainer.id = "grid";
-    gridContainer.style.display = "grid";
-    gridContainer.style.gridTemplateRows = "repeat(32, 1fr)";
-    gridContainer.style.gridTemplateColumns = "repeat(32, 1fr)";
-    for (let j = 1; j < 33; j++) {
-        for (let i = 1; i < 33; i++) {
+function createGrid(size) {
+    gridContainer.style.gridTemplateRows = "repeat(" + size + ", 1fr)";
+    gridContainer.style.gridTemplateColumns = "repeat(" + size + ", 1fr)";
+    for (let j = 1; j < size + 1; j++) {
+        for (let i = 1; i < size + 1; i++) {
             let gridCell = document.createElement("div");
+            gridCell.id = "cell" + i + "," + j;
+            gridCell.classList.add("gridCell");
+
             gridCell.style.gridColumn = i;
             gridCell.style.gridRow = j;
-            gridCell.classList.add("gridCell");
-            gridCell.id = "cell" + i + "," + j;
             gridCell.style.paddingBottom = "100%";
-            gridCell.style.border = "1px dotted black";
 
             gridCell.addEventListener("click", () => {
                 gridCell.classList.toggle("filled");
             })
-            if ((i === 1) || (i === 32) || (j === 1) || (j === 32)) {
-                gridCell.addEventListener("click", () => {
-                    shake()
-                })
-            }
+            /*
+            gridCell.addEventListener("mouseenter", () => {
+                gridCell.classList.add("hover");
+            })
+            gridCell.addEventListener("mouseleave", () => {
+                gridCell.classList.remove("hover");
+            })
+            */
             gridContainer.appendChild(gridCell);
         }
     }
     mainContainer.appendChild(gridContainer);
 }
 
+function hoverEffect(element) {
+
+
+}
 
 function shake() {
     const cells = document.getElementsByClassName("gridCell");
@@ -53,3 +77,18 @@ function shake() {
     }
 }
 
+function changeSize() {
+    let userSize = prompt("How many squares per side?");
+    userSize = parseInt(userSize);
+
+    removeAllChildNodes(gridContainer);
+    mainContainer.removeChild(gridContainer); 
+
+    createGrid(userSize);
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
